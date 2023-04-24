e0d2c9996464e290ebeb3b5339121d74e819a40e
aya <xsdwyyx@126.com>
2023-04-24 15:20:45 +0800

    Initial commit

---
+++ src/test/java/com/example/scd/ScdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
